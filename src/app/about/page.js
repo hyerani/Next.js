3e5a46dc8ef3bc1b9bcb4891a3746e00f5391449
hyerani <hran9404@naver.com>
2023-05-28 23:40:53 +0900

    counter function test

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,13 @@
-export default function Home() {
-  return "Hello about";
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>counter {counter}</h1>
+      <button onClick={() => setCounter((pre) => pre + 1)}>+</button>
+    </div>
+  );
 }
